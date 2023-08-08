0b9d76c157fcd4dcf7c935e069f0f2c871165942
avbm090 <avbm090l@gmail.com>
2023-08-07 21:35:07 -0300

    asd

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 from fastapi import FastAPI
 import pandas as pd
 import numpy as np
-import pickle 
 import matplotlib.pyplot as plt
 import fastparquet
 from sklearn.model_selection import train_test_split
